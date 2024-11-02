c329c3303406aa3eb71a1c8b1dced17b44c3b1b1
Aditya Singh <78498965+adii711@users.noreply.github.com>
2024-11-02 00:23:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="git-ankit",  # Replace with your own username
-    version="1.0.0",
-    author="Suraj,Amit,Jay,Ankit",
-    author_email="tiwariankit1006@gmail.com",
-    description="A recommendation engine",
+    name="MovieRecommender", 
+    version="2.0.1",
+    author="Aditya, Bahare, Monish",
+    author_email="asingh78@ncsu.edu",
+    description="A collaborative filtering-based movie recommendation engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/git-ankit/MovieRecommender",
@@ -17,6 +17,17 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Video",
+    ],
+    install_requires=[
+        "flask", 
+        "flask-mail",
+        "flask-cors",
+        "pytest",
+        "os",
+        # Add other dependencies here as needed
     ],
     python_requires=">=3.6",
 )
